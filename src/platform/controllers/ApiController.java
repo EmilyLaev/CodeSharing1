0a3e99b98ebe5a58ef52ad76c1de93c99d7d70a2
EmilyLaev <laevenea@mcmaster.ca>
2023-02-23 20:23:12 -0500

    Stage five Comments

--- src/platform/controllers/ApiController.java
+++ src/platform/controllers/ApiController.java
@@ -16,6 +16,8 @@ import java.util.Optional;
 public class ApiController {
     private final RepositoryService service;
 
+
+    //Handles GET requests to retrieve a code snippet by UUID
     @Autowired
     public ApiController(RepositoryService service) {
         this.service = service;
@@ -32,23 +34,28 @@ public class ApiController {
                 service.save(snippet);
                 return snippet;
             } else {
+                //If the snippet is not available, delete it from the repository
                 service.delete(snippet);
             }
         }
+        //If the snippet is not found, return a 404 error
         throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 
+    //Handles GET requests to retrieve all code snippets in the repository
     @GetMapping(value = "/api/code/all")
     @ResponseBody
     public Iterable<CodeSnippet> allSnippets() {
         return service.findAll();
     }
 
+    // Handles POST requests to add a new code snippet to the repository
     @PostMapping(value = "/api/code/new", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public String addSnippet(@RequestBody Map<String, Object> map) {
         CodeSnippet snippet = ApiUtils.parseSnippetFromJSON(map);
         service.save(snippet);
+        // Returns the UUID of the new code snippet
         return snippet.getSnippetUUID();
     }
 }
