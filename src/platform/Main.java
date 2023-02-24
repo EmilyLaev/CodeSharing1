71573efcd5ff0d224556a4b36c3f531738d56438
EmilyLaev <laevenea@mcmaster.ca>
2023-02-24 01:34:14 -0500

    Stage five Comments

--- src/platform/Main.java
+++ src/platform/Main.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
+//This is the main program that calls the SpringApplication and runs CodeSharingPlatform
 @SpringBootApplication
 @RestController
 public class CodeSharingPlatform {
